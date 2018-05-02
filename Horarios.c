00d43cbea405892bfb0fb3c3aa22cdd10eab427c
KuroNeko <darkzeus@gmx.es>
2018-05-02 15:52:02 +0200

    Update Horarios.c

--- Horarios.c
+++ Horarios.c
@@ -17,11 +17,11 @@ char fecha() {
 	ctime_s(time, sizeof time, &result);
 	time[4] = '\0';
 	if (time[0] == 'M') dia = 'L';							//Lunes			L
-	else if (time[0] == 'T' && time[1 == 'u']) dia = 'M';	//Martes		M
+	else if (time[0] == 'T' && time[1] == 'u') dia = 'M';	//Martes		M
 	else if (time[0] == 'W') dia = 'X';						//Miercoles		X
-	else if (time[0] == 'T' && time[1 == 'h']) dia = 'J';	//Jueves		J
+	else if (time[0] == 'T' && time[1] == 'h') dia = 'J';	//Jueves		J
 	else if (time[0] == 'F') dia = 'V';						//Viernes		V
-	else if (time[0] == 'S' && time[1 == 'a']) dia = 'S';	//Sabado		S
-	else if (time[0] == 'S' && time[1 == 'u']) dia = 'D';	//Domingo		D
+	else if (time[0] == 'S' && time[1] == 'a') dia = 'S';	//Sabado		S
+	else if (time[0] == 'S' && time[1] == 'u') dia = 'D';	//Domingo		D
 	return(dia);
 }

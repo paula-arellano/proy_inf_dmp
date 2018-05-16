8cd95ef265beff6c901ef63651a02b8d257cd130
KuroNeko <darkzeus@gmx.es>
2018-05-16 11:20:20 +0200

    Update Logo.c

--- Logo.c
+++ Logo.c
@@ -2,88 +2,59 @@
 #include <stdlib.h>
 #include <Windows.h>
 
-//	AVISO: Este codigo esta preparado para funcionar con AltEnter, separarlos puede desembocar en resultados no deseados.
+void logo();
 
-int GetFontSize(HANDLE windowHandle, COORD *size)
-{
-	CONSOLE_FONT_INFOEX font = { sizeof(CONSOLE_FONT_INFOEX) };
-	if (!GetCurrentConsoleFontEx(windowHandle, 0, &font))
-		return 0;
-
-	*size = font.dwFontSize;
-	return 1;
-}
-
-int SetFontSize(HANDLE windowHandle, COORD size)
-{
-	CONSOLE_FONT_INFOEX font = { sizeof(CONSOLE_FONT_INFOEX) };
-	if (!GetCurrentConsoleFontEx(windowHandle, 0, &font))
-		return 0;
-
-	font.dwFontSize = size;
-
-	if (!SetCurrentConsoleFontEx(windowHandle, 0, &font))
-		return 0;
-
-	return 1;
-}
-
-
-void AltEnter(){
-	keybd_event(VK_MENU,		0x38,		0,		0);
-
-	keybd_event(VK_RETURN,		0x1c,		0,		0);
-
-	keybd_event(VK_RETURN,		0x1c,		KEYEVENTF_KEYUP,		0);
-
-	keybd_event(VK_MENU,		0x38,		KEYEVENTF_KEYUP,		0);
-
-	return 0;
+void main() {
+	logo();
 }
 
 void logo() {
-	system("COLOR 70");
-
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD size;
-	if (GetFontSize(hConsole, &size)) {
-		size.X += (SHORT)(size.X * 15);
-		size.Y += (SHORT)(size.Y * 15);
-		SetFontSize(hConsole, size);
-	}
-
-	//TAMAÑO 14 5-ESPACIOS || TAMAÑO 15 4-ESPACIOS
-
-	SetConsoleTextAttribute(hConsole, 240);		//LETRAS NEGRAS EN FONDO BLANCO
-	printf("    HOSPITAL       \n");
 	
-	//SetConsoleTextAttribute(hConsole, 252);		BLANCO
-	//SetConsoleTextAttribute(hConsole, 204);		ROJO
+	printf("        ,,,,,   ,,,,,      ,,,,,,        ,,,,,,     ,,,,,,,,,    ,,,,,  ,,,,,,,,,,,,,     ,,,,,      ,,,,,\n");
+	printf("        MMMMF   JMMMM   ,aMMMMMMMMb,   JMMMM@MMMW,  MMMMMMMMMMb  JMMMM  MMMMMMMMMMMMF    JMMMMM      JMMMM\n");
+	printf("        MMMMF   JMMMM  JMMMMM99NMMMM, JMMMM` 9MMMM  MMMMP!?NMMMF JMMMM  @@@@MMMMM@@@E   JMMMMMMI     JMMMM\n");
+	printf("        MMMMWmmmdMMMM  MMMMP    YMMMW JMMMMMMmJ,    MMMMF  JMMMF JMMMM      MMMMF       MMMMJMMMJ    JMMMM\n");
+	printf("        MMMMMMMMMMMMM  MMMMF    JMMMM  J@MMMMMMMMp  MMMMMMMMMMM+ JMMMM      MMMMF      JMMME,MMMM    JMMMM\n");
+	printf("        MMMMP!!!YMMMM  MMMMF    JMMMM   ,,,7?@MMMML MMMMM@@@9!   JMMMM      MMMMF     JMMMMJJdMMMF   JMMMM\n");
+	printf("        MMMMF   JMMMM  JMMMMp,,JMMMMW JMMMh, ,MMMME MMMMF        JMMMM      MMMMF     MMMMMMMMMMMMJ  JMMMMJJJJJJ\n");
+	printf("        MMMMF   JMMMM   JNMMMMMMMMMW   @MMMMMMMMME  MMMMF        JMMMM      MMMMF    JMMMP^^^^JMMMM  JMMMMMMMMMMF\n");
+	printf("        99999   J9999     ?9@@99`      Q?9@@@9!     MMMMF        JMMMM      MMMMF    9999+     99999 J9999999999T\n");
+	printf(" \n\n\n");
+	SetConsoleTextAttribute(hConsole, 12);
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                   JMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+	printf("                                   JMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+	printf("                                   JMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+	printf("                                   JMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+	printf("                                   JMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n"),
+	printf("                                   JMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+	printf("                                   JMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+	printf("                                   JMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+	printf("                                   J@@@@@@@@@@@@@@@@NMMMMMMMMMMMMMMM@@@@@@@@@@@@@@@@@\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    JMMMMMMMMMMMMMMM\n");
+	printf("                                                    J@@@@@@@@@@@@@@9\n\n\n\n");
 
-	SetConsoleTextAttribute(hConsole, 252); printf("       ");	/* ROJO */	 SetConsoleTextAttribute(hConsole, 204); printf("  ");		/*ROJO*/	SetConsoleTextAttribute(hConsole, 252); printf("        \n");
-	SetConsoleTextAttribute(hConsole, 252); printf("     ");	/* ROJO */	 SetConsoleTextAttribute(hConsole, 204); printf("      ");	/*ROJO*/	SetConsoleTextAttribute(hConsole, 252); printf("        \n");
-	SetConsoleTextAttribute(hConsole, 252); printf("       ");	/* ROJO */	 SetConsoleTextAttribute(hConsole, 204); printf("  ");		/*ROJO*/	SetConsoleTextAttribute(hConsole, 252); printf("        \n");
-
-	SetConsoleTextAttribute(hConsole, 255);
+	SetConsoleTextAttribute(hConsole, 15);
 	system("PAUSE");
-
-	if (GetFontSize(hConsole, &size)) {
-		size.X = (SHORT)(size.X * 0.085);
-		size.Y = (SHORT)(size.Y * 0.085);
-		SetFontSize(hConsole, size);
-	}
-
-	system("color 07");
 	system("cls");
+	system("color 07");
 }
-
-//											VOID MAIN
-void main() {
-
-	AltEnter();
-
-	logo();
-
-	printf("TEST\n");
-	system("PAUSE");
 }
